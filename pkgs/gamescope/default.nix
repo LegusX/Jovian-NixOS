7867e1212016129a43d035fe8f40b31c659e7dc0
Logan Henrie <loghenrie@gmail.com>
2025-10-05 00:49:16 +0000

    Unpinning version in the hopes it fixes build failure

--- pkgs/gamescope/default.nix
+++ pkgs/gamescope/default.nix
@@ -6,14 +6,14 @@
 # NOTE: vendoring gamescope for the time being since we want to match the
 #       version shipped by the vendor, ensuring feature level is equivalent.
 
-gamescope'.overrideAttrs(old: rec {
-  version = "3.16.15";
+# gamescope'.overrideAttrs(old: rec {
+#  version = "3.16.15";
 
-  src = fetchFromGitHub {
-    owner = "ValveSoftware";
-    repo = "gamescope";
-    rev = version;
-    fetchSubmodules = true;
-    hash = "sha256-/JMk1ZzcVDdgvTYC+HQL09CiFDmQYWcu6/uDNgYDfdM=";
-  };
+  #src = fetchFromGitHub {
+    #owner = "ValveSoftware";
+    #repo = "gamescope";
+    #rev = version;
+    #fetchSubmodules = true;
+    #hash = "sha256-/JMk1ZzcVDdgvTYC+HQL09CiFDmQYWcu6/uDNgYDfdM=";
+  #};
 })
